bc53f49d610bc95863f59395156c3a2b789006b2
Saumya-Mishra9129 <saumyamishra9129853131@gmail.com>
2020-06-05 01:43:29 +0530

    Add Gdk version - 3.0

--- LocoSugarActivity.py
+++ LocoSugarActivity.py
@@ -11,6 +11,7 @@
 # Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 import gi
 gi.require_version('Gtk','3.0')
+gi.require_version('Gdk', '3.0')
 from gi.repository import Gtk, GLib, Gdk
 
 from sugar3.activity import activity
